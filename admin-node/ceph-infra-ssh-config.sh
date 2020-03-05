388928365a8b4bd2903ecf3e688b1f1458dbeaca
Igor Baiborodine <igor.baiborodine@yandex.com>
2020-03-04 20:40:12 -0500

    Update ceph-infra-ssh-config.sh

--- admin-node/ceph-infra-ssh-config.sh
+++ admin-node/ceph-infra-ssh-config.sh
@@ -4,4 +4,10 @@ set -ex
 for hostname in "$@"
 do
     ssh-keyscan "$hostname" >> /home/ceph-lab-admin/.ssh/known_hosts
+    ssh-copy-id ceph-lab-admin@"$hostname"
+    {
+      echo "Host $hostname";
+      echo "    Hostname $hostname";
+      echo "    User ceph-lab-admin"
+    } >> /home/ceph-lab-admin/.ssh/config
 done
